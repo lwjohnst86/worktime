170a8bbaf71c87833f09d52fc0baebe7d990c857
Luke Johnston <lwjohnst@gmail.com>
2020-04-22 11:18:14 +0200

    fixed import of org file

--- R/import.R
+++ R/import.R
@@ -14,51 +14,57 @@ count_header_level <- function(x) {
 #' @return Imported org file
 #' @export
 #'
+#' @examples
+#'
+#' import_org_clock("tests/testthat/clocking.org")
+#'
 import_org_clock <- function(file) {
     org_file <- readr::read_lines(file)
 
-    headlines_string <- "^\\*+\\s.*"
+    headlines_string <- rex::rex(
+        start,
+        some_of(rex::escape("*")),
+        space,
+        anything,
+        end
+    )
     timestamps_string <- "CLOCK: "
 
     tasks_times_df <- org_file %>%
-        as_data_frame() %>%
-        filter_at("value",
-                      all_vars(grepl(headlines_string, .) |
-                                      grepl(timestamps_string, .)))
+        as_tibble() %>%
+        filter_at("value", all_vars(grepl(headlines_string, .) |
+                                        grepl(timestamps_string, .)))
 
-    smallest_headline <- max(count_header_level(tasks_times_df$value))
-    if (smallest_headline > 4) {
-        warning("Any headlines below the fourth are not extracted.")
-    }
+    number_headers <- max(count_header_level(tasks_times_df$value))
 
-    headlines_fixed_df <- tasks_times_df %>%
-        mutate_at(
-            "value",
-            funs(
-                AllHeaders = extract_header(., "^\\*+ "),
-                Header1 = extract_header(., "^\\*{1} "),
-                Header2 = extract_header(., "^\\*{2} "),
-                Header3 = extract_header(., "^\\*{3} "),
-                Header4 = extract_header(., "^\\*{4} ")
-            )
-        ) %>%
-        fill("Header1", "Header2", "Header3", "Header4") %>%
-        mutate_at(vars(starts_with("Header"), "value", "AllHeaders"),
-                         funs(gsub("\\*", "", .))) %>%
-        mutate_at("AllHeaders", funs(ifelse(is.na(.), "", .))) %>%
+    headlines_added_df <- tasks_times_df %>%
+        mutate_at("value", list(AllHeaders = ~ extract_header(., "^\\*+ ")))
+
+    headers_separated_df <-
+        1:number_headers %>%
+        purrr::map_dfc(~ extract_header(headlines_added_df$value, paste0("^\\*{", .x, "} ")) %>%
+                           as_tibble() %>%
+                           setNames(paste0("Header", .x))) %>%
+        tidyr::fill(starts_with("Header")) %>%
+        bind_cols(headlines_added_df)
+
+    headers_merged_df <-
+        headers_separated_df %>%
+        mutate_all(~ trimws(gsub("\\*", "", .))) %>%
+        mutate_at("AllHeaders", ~ ifelse(is.na(.), "", .)) %>%
         filter_at("value", any_vars(. != AllHeaders))
 
-    timestamps_extracted_df <- headlines_fixed_df %>%
-        separate(col = "value",
+    timestamps_extracted_df <- headers_merged_df %>%
+        tidyr::separate(col = "value",
                         into = c("ClockIn", "ClockOut"),
                         sep = "--") %>%
         mutate_at(c("ClockIn", "ClockOut"),
-                         funs(trimws(gsub(
+                         list(~ trimws(gsub(
                              "(CLOCK\\: +|\\[|\\].*$)", "", .
                          )))) %>%
         mutate_at(
             c("ClockIn", "ClockOut"),
-            funs(lubridate::parse_date_time),
+            list(lubridate::parse_date_time),
             orders = "ymd a HM",
             tz = Sys.timezone()
         ) %>%
