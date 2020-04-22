7501f94c9aa23ace75de64d899612bd518bf61b1
Luke Johnston <lwjohnst@gmail.com>
2020-04-22 11:18:40 +0200

    create worktime package R metafile

---
+++ R/worktime-package.R
@@ -0,0 +1,15 @@
+#' @keywords internal
+#' @import rlang dplyr ggplot2 grid
+"_PACKAGE"
+
+#' Base ggproto classes for ggepi
+#'
+#' @seealso \link[ggplot2]{ggproto}
+#' @keywords internal
+#' @name worktime-geoms
+NULL
+
+# From ggplot2's utilities.
+"%||%" <- ggplot2:::"%||%"
+
+rex::register_shortcuts("worktime")
